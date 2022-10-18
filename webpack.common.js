da716c634f025fc02e6e66fd284544a284820ba7
Daniel Gallardo <100047357+rolckeirnad@users.noreply.github.com>
2022-10-18 12:32:48 -0500

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,29 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index',
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html',
+  })],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['...', '.jsx', '.scss'],
+  },
+};
