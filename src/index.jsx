2d926b85499da8e67f4d8061e02aaa4995e9a65a
Daniel Gallardo <dgallardo.c@outlook.com>
2022-10-21 23:57:23 -0500

    Add  @material-tailwind

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import './index.css';
+import { ThemeProvider } from '@material-tailwind/react';
 import router from '../router';
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
 );
