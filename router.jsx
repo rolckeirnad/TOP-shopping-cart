b324500d675ff8cd37d9e957e1dfad7e9ebd22a7
Daniel Gallardo <dgallardo.c@outlook.com>
2022-10-23 01:22:12 -0500

    Add Shop route and Product list component

--- router.jsx
+++ router.jsx
@@ -7,16 +7,24 @@ import {
 
 import App from './src/App';
 import Home from './src/routes/Home';
+import Shop from './src/routes/Shop';
+import ProductsList from './src/components/ProductsList';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route
+      exact
       path="/"
       element={<App />}
     >
       <Route errorElement={<div>Error screen</div>}>
         <Route index element={<Home />} />
-        <Route path="shop" element={<div>Shop</div>} />
+        <Route path="shop" element={<Shop />}>
+          <Route path="" element={<ProductsList />} />
+          <Route path="electronics" element={<ProductsList />} />
+          <Route path="home" element={<ProductsList />} />
+          <Route path="pets" element={<ProductsList />} />
+        </Route>
         <Route path="about" element={<div>About</div>} />
       </Route>
     </Route>,
