a94e6b46a72c1cdc6b454d88889bb26315cc448c
Daniel Gallardo <dgallardo.c@outlook.com>
2022-11-04 01:08:59 -0600

    Add import of Item component

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -10,38 +10,7 @@ import {
 } from '@material-tailwind/react';
 import { Link, useLoaderData } from 'react-router-dom';
 import { fetchNewProducts } from '../fake-store';
-
-function Item({ data }) {
-  const {
-    id, title, price, description, image,
-  } = data;
-  return (
-    <Card className="relative justify-around flex-grow flex-shrink-0 basis-[calc(33.33%-4px)] snap-center border" id={id}>
-      <CardHeader className="relative mt-4 h-56">
-        <img
-          src={image}
-          className="block w-full"
-          alt="product"
-        />
-      </CardHeader>
-      <CardBody className="text-center">
-        <Typography variant="h5" className="home-title-clamp h-[3.3rem] text-xl mb-2">
-          {title}
-        </Typography>
-        <Typography variant="paragraph" className="home-line-clamp">
-          {description}
-        </Typography>
-      </CardBody>
-      <CardFooter divider className="flex flex-col items-center justify-between p-0 mb-4">
-        <Typography variant="small" color="indigo">
-          Price: $
-          {price}
-        </Typography>
-        <Button disabled>Add to Cart</Button>
-      </CardFooter>
-    </Card>
-  );
-}
+import Item from '../components/Item';
 
 const newProductsQuery = () => ({
   queryKey: ['home'],
