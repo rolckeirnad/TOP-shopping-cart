7ccac6e03a46844cae611272c478a6027f94ea9f
Daniel Gallardo <dgallardo.c@outlook.com>
2022-11-04 20:07:57 -0600

    Replace menu entries with categories from API

--- src/routes/Shop.jsx
+++ src/routes/Shop.jsx
@@ -1,15 +1,27 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { fetchCategories } from '../fake-store';
+
+const categoriesQuery = () => ({
+  queryKey: ['categories'],
+  queryFn: async () => fetchCategories(),
+});
+
+export const loader = (queryClient) => async () => {
+  const query = categoriesQuery();
+  await queryClient.prefetchQuery({ ...query, staleTime: Infinity, refetchOnMount: false });
+};
 
 function Shop() {
+  const { data: categories } = useQuery({ ...categoriesQuery(), staleTime: Infinity });
+
   return (
     <div className="grid grid-cols-5 h-full">
       <div className="col-span-1 pl-5 bg-deep-orange-100">
         <h1 className="text-xl text-center sm:text-3xl leading-4">Categories</h1>
         <Link to="/shop"><h2 className="text-lg border-gray-600 border-b-2 hover:bg-orange-600">New</h2></Link>
-        <Link to="/shop/electronics"><h2 className="text-lg border-gray-600 border-b-2 hover:bg-orange-600">Electronics</h2></Link>
-        <Link to="/shop/home"><h2 className="text-lg border-gray-600 border-b-2 hover:bg-orange-600">Home</h2></Link>
-        <Link to="/shop/pets"><h2 className="text-lg border-gray-600 border-b-2 hover:bg-orange-600">Pets</h2></Link>
+        {categories.map((category, i) => <Link to={`${category}`} key={`${category}-${i}`}><h2 className="text-lg border-gray-600 border-b-2 hover:bg-orange-600">{category}</h2></Link>)}
       </div>
       <Outlet />
     </div>
