fd9d8ab2d547d6a15c56f53b391cdf843be8acd5
Daniel Gallardo <dgallardo.c@outlook.com>
2022-11-08 01:33:40 -0600

    Update link path

--- src/routes/Shop.jsx
+++ src/routes/Shop.jsx
@@ -27,7 +27,7 @@ function Shop() {
     <div className="grid grid-cols-5 h-full overflow-hidden">
       <div className="col-span-1 pl-5 bg-deep-orange-100">
         <h1 className="text-xl text-center sm:text-3xl leading-4">Categories</h1>
-        <Link to="/shop"><h2 className="text-lg border-gray-600 border-b-2 hover:bg-orange-600">New</h2></Link>
+        <Link to="/shop/all"><h2 className="text-lg border-gray-600 border-b-2 hover:bg-orange-600">New</h2></Link>
         {categories.map((category, i) => <Link to={`${category}`} key={`${category}-${i}`}><h2 className="text-lg border-gray-600 border-b-2 hover:bg-orange-600">{category}</h2></Link>)}
       </div>
       <Outlet />
