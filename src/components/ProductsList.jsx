2d6618e28d302439e37d9198429f4179982e01b3
Daniel Gallardo <dgallardo.c@outlook.com>
2022-11-08 21:59:09 -0600

    Update ProductsList component

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,18 +1,36 @@
-import { Breadcrumbs } from '@material-tailwind/react';
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import {
+  useParams,
+} from 'react-router-dom';
+import { fetchSpecificCategory } from '../fake-store';
+import { productsQuery } from '../routes/Shop';
+import ItemPlaceholder from './ItemPlaceholder';
+import ItemShop from './ItemShop';
+
+const categoryQuery = (category) => ({
+  queryKey: [`${category}`],
+  queryFn: async () => fetchSpecificCategory(encodeURI(category)),
+});
 
 function ProductsList() {
-  const location = useLocation();
-  const pathLinks = location.pathname.split('/');
+  const { categoryPath } = useParams();
+  const query = categoryPath ? categoryQuery(categoryPath) : productsQuery();
+  const { data: products, isSuccess } = useQuery({
+    ...query, staleTime: Infinity, useErrorBoundary: true, refetchOnMount: false,
+  });
   return (
-    <div>
-      <Breadcrumbs>
-        {pathLinks.map((url) => <Link to={`/${url}`} className="capitalize">{`${url}`}</Link>)}
-      </Breadcrumbs>
-      <div>Grid</div>
+    <div className="col-start-2 col-end-[-1] overflow-y-auto">
+      <div className="grid grid-cols-4 grid-flow-row gap-8 px-6 py-10">
+        {!isSuccess && (Array(8).fill('')
+          .map((_, ind) => <ItemPlaceholder key={`placeholder-${ind}`} />)
+        )}
+        {isSuccess && (products.map((product) => (
+          <ItemShop product={product} />
+        ))
+        )}
+      </div>
     </div>
   );
 }
-
 export default ProductsList;
