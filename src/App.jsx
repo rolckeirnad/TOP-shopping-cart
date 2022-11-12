b14dbc811be9de36b8553cadd13ffc1be8b9cabc
Daniel Gallardo <dgallardo.c@outlook.com>
2022-11-12 11:36:05 -0600

    Fix duplicate cart items

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,15 @@ function App() {
 
   const addToCart = (product, quantity) => {
     setCart((prevCart) => {
+      // Search product in cart by id
+      const index = prevCart.findIndex((item) => item.id === product.id);
+      // If found, update quantity with new quantity
+      if (index > -1) {
+        const newCart = [...prevCart];
+        newCart[index] = { ...product, quantity: prevCart[index].quantity + quantity };
+        return newCart;
+      }
+      // If not, just add the item to cart.
       const newCart = [...prevCart, { ...product, quantity }];
       return newCart;
     });
