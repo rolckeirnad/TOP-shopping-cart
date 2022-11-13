efe18f2822b02f7de2ccabacc19c6ac68a5bedde
Daniel Gallardo <dgallardo.c@outlook.com>
2022-11-12 21:36:02 -0600

    Update sidebar

--- src/routes/Shop.jsx
+++ src/routes/Shop.jsx
@@ -1,7 +1,19 @@
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  fas, faPerson, faComputer, faGem, faPersonDress, faCircleExclamation,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-import { Link, Outlet, useOutletContext } from 'react-router-dom';
+import {
+  NavLink, Outlet, useOutletContext,
+} from 'react-router-dom';
 import { fetchCategories, fetchProducts } from '../fake-store';
+
+library.add(fas);
+
+const customIcons = [faComputer, faGem, faPerson, faPersonDress];
 
 const categoriesQuery = () => ({
   queryKey: ['categories'],
@@ -34,10 +46,17 @@ function Shop() {
   const addToCart = useOutletContext();
   return (
     <div className="grid grid-cols-5 h-full overflow-hidden">
-      <div className="col-span-1 pl-5 bg-deep-orange-100">
-        <h1 className="text-xl text-center sm:text-3xl leading-4">Categories</h1>
-        <Link to="/shop/all"><h2 className="text-lg border-gray-600 border-b-2 hover:bg-orange-600">New</h2></Link>
-        {categories.map((category, i) => <Link to={`/shop/${encodeURI(category)}`} key={`${category}-${i}`}><h2 className="text-lg border-gray-600 border-b-2 hover:bg-orange-600">{category}</h2></Link>)}
+      <div className="col-span-1 py-10 capitalize text-white bg-gray-800 text-2xl flex flex-col gap-6">
+        <NavLink to="/shop/all" className={({ isActive }) => (isActive ? 'flex items-center pl-2 bg-orange-600 ease-in-out duration-300' : 'flex items-center pl-2 ease-in-out duration-300')}>
+          <FontAwesomeIcon icon={icon({ name: 'star', style: 'solid' })} />
+          <h2 className="flex-1 ml-5 ease-in-out duration-300 hover:scale-105">New</h2>
+        </NavLink>
+        {categories && categories.map((category, i) => (
+          <NavLink to={`/shop/${encodeURI(category)}`} className={({ isActive }) => (isActive ? 'flex items-center pl-2 bg-orange-600 ease-in-out duration-300' : 'flex items-center pl-2 ease-in-out duration-300')} key={`${category}-sidebar-entry`}>
+            <FontAwesomeIcon icon={customIcons[i] || faCircleExclamation} />
+            <h2 className="flex-1 ml-5 ease-in-out duration-300 hover:scale-105">{category}</h2>
+          </NavLink>
+        ))}
       </div>
       <Outlet context={addToCart} />
     </div>
